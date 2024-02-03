729f26253f6f4123d115842dfd782dd29fe0b3dd
Erkin Tekin <erkintekin12@gmail.com>
2024-02-04 00:38:01 +0300

    Equipment page and CRUDs added

--- Holding/Program.cs
+++ Holding/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.C
 
 builder.Services.AddScoped<ISkillService, SkillManager>();
 builder.Services.AddScoped<IRepository<Skill>, GenericRepository<Skill>>();
+builder.Services.AddScoped<IEquipmentService, EquipmentManager>();
+builder.Services.AddScoped<IRepository<Equipment>, GenericRepository<Equipment>>();
 
 var app = builder.Build();
 
